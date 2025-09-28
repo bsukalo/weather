c19a20b02c2f5b469c0618f656ebe0d8e30b3038
bsukalo <bsukalo@outlook.com>
2025-09-28 23:27:06 +0200

    Add searchBar component

--- src/components/mainWindow/MainWindow.tsx
+++ src/components/mainWindow/MainWindow.tsx
@@ -1,9 +1,11 @@
 import CurrentWeatherWindow from "../currentWeatherWindow/CurrentWeatherWindow";
 import "./MainWindow.css";
+import SearchBar from "../searchBar/SearchBar.tsx";
 
 const MainWindow = () => {
   return (
     <div className="upper-layer-container">
+      <SearchBar />
       <div className="upper-layer">
         <CurrentWeatherWindow />
       </div>
---
+++ src/components/searchBar/SearchBar.tsx
@@ -0,0 +1,13 @@
+import "./SearchBar.css";
+
+const SearchBar = () => {
+  return (
+    <div className="searchbar-container">
+      <div className="searchbar">
+        <div className="searchbar-text">Search for a location...</div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
