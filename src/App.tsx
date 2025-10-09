d254715f2f7cd91164ca0839e859e28f43ade6bc
bsukalo <bsukalo@outlook.com>
2025-10-09 21:48:09 +0200

    Move background into mainWindow component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import Background from "./components/background/Background.tsx";
 import MainWindow from "./components/mainWindow/MainWindow.tsx";
 
 function App() {
   return (
     <>
-      <Background />
       <MainWindow />
     </>
   );
