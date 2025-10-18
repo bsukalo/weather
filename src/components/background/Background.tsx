aa7e99b8bf2db5638dadf62371feb7f157896ae4
bsukalo <bsukalo@outlook.com>
2025-10-18 22:38:20 +0200

    Add smooth transitions to dynamic background

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "./Background.css";
 
 interface Props {
@@ -18,12 +19,35 @@ const Background = ({ weather, time }: Props) => {
   const locationTime = time;
   let condition = (weather?.toLowerCase().replace(/ /g, "") || "clear") + dayOrNight(locationTime) + ".png";
   const imageURL = new URL(`../../assets/${condition}`, import.meta.url).href;
+  const [isTransitioning, setTransitioning] = useState(false);
+  const [currentBg, setCurrentBg] = useState<String | null>(imageURL);
+  const [nextBg, setNextBg] = useState<String | null>(imageURL);
 
-  return <div
-    className="dynamic-background"
-    style={{ backgroundImage: `url(${imageURL})` }}
-  >
-  </div >;
+  console.log(currentBg);
+
+  useEffect(() => {
+    setNextBg(imageURL);
+    setTransitioning(true);
+    setTimeout(() => {
+      setCurrentBg(imageURL);
+      setTransitioning(false);
+    }, 700)
+  }, [imageURL])
+
+  return <>
+    <div
+      className="dynamic-background"
+      style={{ backgroundImage: `url(${currentBg})`, opacity: `${isTransitioning ? '0' : '1'}` }
+      }
+    >
+    </div >
+    <div
+      className="dynamic-background next"
+      style={{ backgroundImage: `url(${nextBg})` }
+      }
+    >
+    </div >
+  </>;
 };
 
 export default Background;
