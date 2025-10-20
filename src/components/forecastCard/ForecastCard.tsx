1500ca203badb47c1dd2279eeb13d16995e19832
bsukalo <bsukalo@outlook.com>
2025-10-20 20:01:44 +0200

    Remove unused skeletons

--- src/components/forecastCard/ForecastCard.tsx
+++ src/components/forecastCard/ForecastCard.tsx
@@ -1,4 +1,3 @@
-import Skeleton from "../skeleton/Skeleton";
 import "./ForecastCard.css";
 
 interface Props {
@@ -28,45 +27,27 @@ const ForecastCard = ({
       <div className="forecast-card">
         <div className="forecast-left-container">
           <div className="forecast-day">{forecast_day}</div>
-          {weather_description ? (
-            <div className="forecast-left">
-              <div className="forecast-description">{weather_description}</div>
-              <img className="forecast-icon" src={forecast_icon}></img>
-            </div>
-          ) : (
-            <div className="forecast-description">
-              <Skeleton skeletonWidth="200px" skeletonHeight="1em" />
-            </div>
-          )}
+          <div className="forecast-left">
+            <div className="forecast-description">{weather_description}</div>
+            <img className="forecast-icon" src={forecast_icon}></img>
+          </div>
         </div>
         <div className="forecast-right-container">
           <div className="forecast-right">
             <div className="min-temperature">
               Min
               <br />
-              {min_temperature ? (
-                <div className="forecast-temperature">{min_temperature}°</div>
-              ) : (
-                <Skeleton skeletonWidth="50px" skeletonHeight="50px" />
-              )}
+              <div className="forecast-temperature">{min_temperature}°</div>
             </div>
             <div className="avg-temperature">
               Avg
               <br />
-              {avg_temperature ? (
-                <div className="forecast-temperature">{avg_temperature}°</div>
-              ) : (
-                <Skeleton skeletonWidth="50px" skeletonHeight="50px" />
-              )}
+              <div className="forecast-temperature">{avg_temperature}°</div>
             </div>
             <div className="max-temperature">
               Max
               <br />
-              {max_temperature ? (
-                <div className="forecast-temperature">{max_temperature}°</div>
-              ) : (
-                <Skeleton skeletonWidth="50px" skeletonHeight="50px" />
-              )}
+              <div className="forecast-temperature">{max_temperature}°</div>
             </div>
           </div>
         </div>
