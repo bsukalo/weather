c1fe5e86d73a14d7b09252e5d20b5e08d4289564
bsukalo <bsukalo@outlook.com>
2025-11-06 23:22:58 +0100

    make api calls over https

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiKey = import.meta.env.VITE_API_KEY;
 
 export default axios.create({
-  baseURL: "http://api.weatherapi.com/v1",
+  baseURL: "https://api.weatherapi.com/v1",
   params: {
     key: apiKey,
   },
